b4c761f8ab51e62dc220ab5a378d3234578e8de1
Bastien Jansen <bastien.jansen@gmx.com>
2020-07-12 13:40:01 +0200

    Improved presentation of the code style panel

--- src/main/kotlin/com/github/bjansen/intellij/pebble/codeStyle/DelimitersConfigurationPanel.kt
+++ src/main/kotlin/com/github/bjansen/intellij/pebble/codeStyle/DelimitersConfigurationPanel.kt
@@ -7,7 +7,9 @@ import com.intellij.psi.codeStyle.CustomCodeStyleSettings
 import com.intellij.psi.codeStyle.LanguageCodeStyleSettingsProvider
 import com.intellij.ui.OptionGroup
 import com.intellij.ui.components.JBTextField
-import java.awt.GridLayout
+import java.awt.GridBagConstraints
+import java.awt.GridBagLayout
+import javax.swing.BorderFactory
 import javax.swing.JLabel
 import javax.swing.JPanel
 
@@ -16,11 +18,11 @@ import javax.swing.JPanel
  */
 class DelimitersConfigurationPanel(settings: CodeStyleSettings) : CustomizableLanguageCodeStylePanel(settings) {
 
-    private val myPanel = JPanel(GridLayout(0, 1))
-    private val tagOpen = JBTextField("{%")
-    private val tagClose = JBTextField("%}")
-    private val printOpen = JBTextField("{{")
-    private val printClose = JBTextField("}}")
+    private val myPanel = JPanel(GridBagLayout())
+    private val tagOpen = JBTextField("{%", 10)
+    private val tagClose = JBTextField("%}", 10)
+    private val printOpen = JBTextField("{{", 10)
+    private val printClose = JBTextField("}}", 10)
 
     init {
         init()
@@ -29,17 +31,29 @@ class DelimitersConfigurationPanel(settings: CodeStyleSettings) : CustomizableLa
     override fun init() {
         super.init()
 
+        val constraints = GridBagConstraints()
+        constraints.anchor = GridBagConstraints.NORTHWEST
+        constraints.gridwidth = 700
+        constraints.gridx = 0
+        constraints.weightx = 1.0
+        constraints.weighty = 0.0
+
         val tagsOptionGroup = OptionGroup("Tag directives")
         tagsOptionGroup.add(JLabel("Opening delimiter"), tagOpen)
         tagsOptionGroup.add(JLabel("Closing delimiter"), tagClose)
 
-        myPanel.add(tagsOptionGroup.createPanel())
+        constraints.gridy = 0
+        myPanel.add(tagsOptionGroup.createPanel(), constraints)
 
         val printOptionGroup = OptionGroup("Print directives")
         printOptionGroup.add(JLabel("Opening delimiter"), printOpen)
         printOptionGroup.add(JLabel("Closing delimiter"), printClose)
 
-        myPanel.add(printOptionGroup.createPanel())
+        constraints.weighty = 1.0
+        constraints.gridy = 1
+        myPanel.add(printOptionGroup.createPanel(), constraints)
+
+        myPanel.border = BorderFactory.createEmptyBorder(0, 10, 0, 0)
     }
 
     override fun getTabTitle() = "Delimiters"
