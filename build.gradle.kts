d643a84098b3897fb29abd299b69403ef9f68d06
Bastien Jansen <bastien.jansen@gmx.com>
2022-04-23 20:07:42 +0200

    Update dependencies

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,11 +1,10 @@
-import org.jetbrains.intellij.tasks.PublishTask
+import org.jetbrains.intellij.tasks.PublishPluginTask
 import org.jetbrains.intellij.tasks.RunPluginVerifierTask
 import kotlin.collections.listOf
 
 val ideaVersion: String by project
 val downloadIdeaSources: String by project
-val publishUsername: String by project
-val publishPassword: String by project
+val publishToken: String by project
 val publishChannels: String by project
 
 buildscript {
@@ -16,7 +15,7 @@ buildscript {
 }
 
 plugins {
-    id("org.jetbrains.intellij") version "0.7.2"
+    id("org.jetbrains.intellij") version "1.5.3"
     id("org.sonarqube") version "3.3"
     kotlin("jvm") version "1.3.72"
     jacoco
@@ -40,19 +39,18 @@ project(":") {
     }
 
     intellij {
-        version = ideaVersion
-        downloadSources = downloadIdeaSources.toBoolean()
-        updateSinceUntilBuild = false
-        setPlugins("Spring", "java-i18n", "properties", "java", "IntelliLang")
+        version.set(ideaVersion)
+        downloadSources.set(downloadIdeaSources.toBoolean())
+        updateSinceUntilBuild.set(false)
+        plugins.set(listOf("Spring", "java-i18n", "properties", "java", "IntelliLang"))
 
-        tasks.withType<PublishTask> {
-            username(publishUsername)
-            password(publishPassword)
-            channels(publishChannels)
+        tasks.withType<PublishPluginTask> {
+            token.set(publishToken)
+            channels.set(publishChannels.split(','))
         }
 
         tasks.withType<RunPluginVerifierTask> {
-            setIdeVersions(listOf("IU-2019.2.3", "IU-2020.3.2"))
+            ideVersions.set(listOf("IU-2019.2.3", "IU-2020.3.2"))
         }
     }
 
